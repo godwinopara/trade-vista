114dbd2f2fbdc3b5d057f75916a8914f7c2f28f0
stivex001 <stephenadeyemo@gmail.com>
2024-04-19 04:19:54 +0100

    feat:implemented meta4 page

---
+++ src/pages/Metatrader5.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/layouts/MainLayout'
+
+type Props = {}
+
+const Metatrader5 = (props: Props) => {
+  return (
+    <MainLayout>Metatrader5</MainLayout>
+  )
+}
+
+export default Metatrader5
