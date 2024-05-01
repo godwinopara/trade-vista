662a936c217f54bcf8c97cbfaa315add38c6f94d
Godwin Opara <godwinopara62@yahoo.com>
2024-05-01 18:10:34 +0100

    fix git guardian

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDSH7s_vN63klijoB0e7ivAb8jyx5F5LPE",
-	authDomain: "investinspire-3b5e4.firebaseapp.com",
-	projectId: "investinspire-3b5e4",
-	storageBucket: "investinspire-3b5e4.appspot.com",
-	messagingSenderId: "861782288484",
-	appId: "1:861782288484:web:101c77644ccafe960ed771",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
