f4b805750f6afe8ae686bde6dd52cda4acad0788
stivex001 <stephenadeyemo@gmail.com>
2024-05-17 11:21:25 +0100

    feat:user dashboard styles fixed

--- src/components/dashboards/VerificationCard.tsx
+++ src/components/dashboards/VerificationCard.tsx
@@ -3,34 +3,40 @@ import { useUserContext } from "../../context/UserContext";
 import { useEffect, useState } from "react";
 
 export default function VerificationCard() {
-	const [verified, setVerified] = useState(false);
-	const [verificationStatus, setVerificationStatus] = useState("not-verified");
+  const [verified, setVerified] = useState(false);
+  const [verificationStatus, setVerificationStatus] = useState("not-verified");
 
-	// const { verification } = useUserContext().state;
-	// // const verified = state.verification?.status === "verified" ? true : false;
+  // const { verification } = useUserContext().state;
+  // // const verified = state.verification?.status === "verified" ? true : false;
 
-	// useEffect(() => {
-	// 	setVerificationStatus(verification.status);
-	// 	setVerified(verification.status === "verified");
-	// 	// eslint-disable-next-line
-	// }, [verification.status]);
+  // useEffect(() => {
+  // 	setVerificationStatus(verification.status);
+  // 	setVerified(verification.status === "verified");
+  // 	// eslint-disable-next-line
+  // }, [verification.status]);
 
-	// console.log(state);
+  // console.log(state);
 
-	return (
-		<div className="w-full rounded-md border border-stroke  bg-primary text-white py-4 px-7 shadow-default ">
-			<div className="w-full my-2 flex items-end justify-between">
-				<div>
-					<span className="text-xl font-semibold mb-2 block">Verification Status</span>
+  return (
+    <div className="w-full rounded-md border border-strokedark  bg-boxdark text-white py-4 px-7 shadow-default ">
+      <div className="w-full my-2 flex items-end justify-between">
+        <div>
+          <span className="text-xl font-semibold mb-2 block">
+            Verification Status
+          </span>
 
-					<h4 className="text-lg font-medium text-white mb-1 flex items-center gap-x-2">
-						<span className={`${verified ? "text-meta-3" : "text-warning"} text-2xl`}>
-							<MdVerified />
-						</span>
-						{verificationStatus[0].toUpperCase() + verificationStatus.slice(1)}
-					</h4>
-				</div>
-			</div>
-		</div>
-	);
+          <h4 className="text-lg font-medium text-white mb-1 flex items-center gap-x-2">
+            <span
+              className={`${
+                verified ? "text-meta-3" : "text-warning"
+              } text-2xl`}
+            >
+              <MdVerified />
+            </span>
+            {verificationStatus[0].toUpperCase() + verificationStatus.slice(1)}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/UserDashboard.tsx
+++ src/pages/UserDashboard.tsx
@@ -11,60 +11,74 @@ import Loader from "../components/ui/Loader";
 type Props = {};
 
 const UserDashboard = (props: Props) => {
-	const { state, loading } = useUserContext();
+  const { state, loading } = useUserContext();
 
-	const totalWithdrawal = state.withdrawals?.reduce(
-		(acc, curr) => (curr.status === "Completed" ? acc + Number(curr.amount) : 0),
-		0
-	);
-	const totalDeposit = state.deposits?.reduce(
-		(acc, curr) => (curr.status === "Completed" ? acc + Number(curr.amount) : 0),
-		0
-	);
+  const totalWithdrawal = state.withdrawals?.reduce(
+    (acc, curr) =>
+      curr.status === "Completed" ? acc + Number(curr.amount) : 0,
+    0
+  );
+  const totalDeposit = state.deposits?.reduce(
+    (acc, curr) =>
+      curr.status === "Completed" ? acc + Number(curr.amount) : 0,
+    0
+  );
 
-	return (
-		<AdminLayout>
-			{loading ? (
-				<Loader />
-			) : (
-				<div className="flex flex-col gap-6">
-					<div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
-						<CardDataStats
-							title="Total Balance"
-							totalUsd={` ${state.account.balance}`}
-							totalBtc={(Number(state.account.balance) / state.bitcoin).toFixed(8)}
-						/>
-						<CardDataStats
-							title="Total Profit"
-							totalUsd={` ${state.account.profit}`}
-							totalBtc={(Number(state.account.profit) / state.bitcoin).toFixed(8)}
-						/>
-						<CardDataStats
-							title="Total Bonus"
-							totalUsd={` ${state.account.bonus}`}
-							totalBtc={(Number(state.account.bonus) / state.bitcoin).toFixed(8)}
-						/>
-					</div>
-					<div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
-						<CardDataStats title="Total Deposit" totalUsd={` ${totalDeposit}`} />
-						<CardDataStats title="Total Withdrawal" totalUsd={` ${totalWithdrawal}`} />
-						<VerificationCard />
-					</div>
-					<div className=" grid grid-cols-12 gap-4 md:gap-6 ">
-						<div className=" flex flex-col gap-5 col-span-12 xl:col-span-8">
-							<StockMarket height={700} width="100%" showChart></StockMarket>
-						</div>
-						<div className="col-span-12 xl:col-span-4 flex flex-col gap-5">
-							<TradingSession />
-						</div>
-					</div>
-					<div className="col-span-12 xl:col-span-4 flex flex-col gap-5">
-						<DashboardTable />
-					</div>
-				</div>
-			)}
-		</AdminLayout>
-	);
+  return (
+    <AdminLayout>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col gap-6">
+          <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
+            <CardDataStats
+              title="Total Balance"
+              totalUsd={` ${state.account.balance}`}
+              totalBtc={(Number(state.account.balance) / state.bitcoin).toFixed(
+                8
+              )}
+            />
+            <CardDataStats
+              title="Total Profit"
+              totalUsd={` ${state.account.profit}`}
+              totalBtc={(Number(state.account.profit) / state.bitcoin).toFixed(
+                8
+              )}
+            />
+            <CardDataStats
+              title="Total Bonus"
+              totalUsd={` ${state.account.bonus}`}
+              totalBtc={(Number(state.account.bonus) / state.bitcoin).toFixed(
+                8
+              )}
+            />
+          </div>
+          <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 ">
+            <CardDataStats
+              title="Total Deposit"
+              totalUsd={` ${totalDeposit}`}
+            />
+            <CardDataStats
+              title="Total Withdrawal"
+              totalUsd={` ${totalWithdrawal}`}
+            />
+            <VerificationCard />
+          </div>
+          <div className=" grid grid-cols-12 gap-4 md:gap-6 ">
+            <div className=" flex flex-col gap-5 col-span-12 xl:col-span-8">
+              <StockMarket colorTheme="dark" height={700} width="100%" showChart></StockMarket>
+            </div>
+            <div className="col-span-12 xl:col-span-4 flex flex-col gap-5">
+              <TradingSession />
+            </div>
+          </div>
+          <div className="col-span-12 xl:col-span-4 flex flex-col gap-5">
+            <DashboardTable />
+          </div>
+        </div>
+      )}
+    </AdminLayout>
+  );
 };
 
 export default UserDashboard;
